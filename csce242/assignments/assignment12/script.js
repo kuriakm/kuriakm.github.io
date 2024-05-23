53caf3ba6481a0aa624f4e8359b9f27d247c0b97
kuriakm <kuriakose_ma@mailfence.com>
2024-05-23 10:23:03 -0400

    Added assignments/assignment12

---
+++ csce242/assignments/assignment12/script.js
@@ -0,0 +1,42 @@
+// https://portiaportia.github.io/json/ice-creams.json
+const getIceCreams = async () => {
+    try {
+        return (await fetch("https://portiaportia.github.io/json/ice-creams.json")).json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const showIceCreams = async () => {
+    const iceCreams = (await getIceCreams());
+    const iceCreamList = document.getElementById("icecream-list");
+
+    iceCreams.forEach(async (iceCream) => {
+        iceCreamList.append(getIceCream(iceCream));
+    });
+};
+
+const getIceCream = (iceCream) => {
+    const section = document.createElement("section");
+    section.classList.add("container");
+
+    // Image
+    const img = document.createElement("img");
+    img.src = "https://portiaportia.github.io/json/images/ice-creams/" + iceCream.image;
+    section.append(img);
+
+    // Overlay
+    const div = document.createElement("div");
+    div.classList.add("overlay");
+
+    // Overlay Text
+    const overlayText = document.createElement("p");
+    overlayText.innerHTML = iceCream.name;
+    div.append(overlayText);
+
+    section.append(div);
+
+    return section;
+};
+
+window.onload = showIceCreams();
