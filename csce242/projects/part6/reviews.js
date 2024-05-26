bd04197f3f6ab865a21fe8ec4f4dd8a42bef4237
kuriakm <kuriakose_ma@mailfence.com>
2024-05-26 15:44:29 -0400

    Created working review submission form

--- csce242/projects/part6/reviews.js
+++ csce242/projects/part6/reviews.js
@@ -19,100 +19,84 @@ const showProduct = async () => {
 
     products.forEach((product) => {
         if (product.item == itemType) {
-            const mainDiv = document.createElement("div");
+            const reviewDiv = document.createElement("div");
 
             const reviewLength = Object.keys(product.reviews).length;
             if (reviewLength > 0) {
                 for (let i = 0; i < reviewLength; i++) {
-                    const review = document.createElement("div");
-                    review.classList.add("reviews");
-
-                    const reviewHeader = document.createElement("div");
-                    reviewHeader.classList.add("review-head");
-
-                    const reviewer = document.createElement("h4");
-                    reviewer.innerHTML = product.reviews[i].reviewer;
-
-                    reviewHeader.append(reviewer);
-                    review.append(reviewHeader);
-
-                    const reviewContent = document.createElement("p");
-                    reviewContent.innerHTML = product.reviews[i].content;
-                    review.append(reviewContent);
-
-                    const reviewRating = document.createElement("p");
-                    reviewRating.classList.add("rating");
-                    reviewRating.innerHTML = product.reviews[i].rating + "/5";
-                    review.append(reviewRating);
-
-                    mainDiv.append(review)
+                    reviewDiv.append(getReview(product.reviews[i]));
                 }
             }
-            reviews.append(mainDiv);
+            reviews.append(reviewDiv);
         }
     });
 };
 
+// Get review to append to review-box
+const getReview = (review) => {
+    const reviewSection = document.createElement("div");
+    reviewSection.classList.add("reviews");
+
+    const reviewHeader = document.createElement("div");
+    reviewHeader.classList.add("review-head");
+
+    const reviewer = document.createElement("h4");
+    reviewer.innerHTML = review.reviewer;
+
+    reviewHeader.append(reviewer);
+    reviewSection.append(reviewHeader);
+
+    const reviewContent = document.createElement("p");
+    reviewContent.innerHTML = review.content;
+    reviewSection.append(reviewContent);
+
+    const reviewRating = document.createElement("p");
+    reviewRating.classList.add("rating");
+    reviewRating.innerHTML = review.rating + "/5";
+    reviewSection.append(reviewRating);
+    return reviewSection;
+}
+
+// Review form submission
+document.getElementById("review-form").onsubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.elements["txt-name"].value;
+    const review = form.elements["txt-com"].value;
+    const rating = form.elements["rating"].value;
+    const result = document.getElementById("result");
+
+    // Append review to review-box
+    try {
+        const reviewSection = document.createElement("div");
+        reviewSection.classList.add("reviews");
+
+        const reviewHeader = document.createElement("div");
+        reviewHeader.classList.add("review-head");
+
+        const reviewer = document.createElement("h4");
+        reviewer.innerHTML = name;
+
+        reviewHeader.append(reviewer);
+        reviewSection.append(reviewHeader);
+
+        const reviewContent = document.createElement("p");
+        reviewContent.innerHTML = review;
+        reviewSection.append(reviewContent);
+
+        const reviewRating = document.createElement("p");
+        reviewRating.classList.add("rating");
+        reviewRating.innerHTML = rating + "/5";
+        reviewSection.append(reviewRating);
+
+        document.getElementById("review-box").append(reviewSection);
+        result.innerHTML = "Review successfully submitted.";
+    } catch (error) {
+        console.log(error);
+        result.innerHTML = "Sorry, there was an issue submitting your review.";
+    }
+
+
+}
+
 window.onload = showProduct();
-
-/* <section id="store-item-page">
-                <h2>Pixel Renaissance Stickers</h2>
-                <img id="store-thumb" src="images/sticker-thumb.jpg">
-
-                <!-- Store Item Preview pictures box -->
-                <!-- Change main store item picture when store-items-pics section clicked -->
-                <section id="store-item-pics" class="columns wrap three">
-                    <section id="store-items" class="one">
-                        <img src="images/sticker1.jpg">
-                    </section>
-                    <section id="store-items" class="one">
-                        <img src="images/sticker2.jpg">
-                    </section>
-                    <section id="store-items" class="one">
-                        <img src="images/sticker3.jpg">
-                    </section>
-                </section>
-                <h3>$5.99</h3>
-                <h4>Logo by Marshall Kuriakose</h4>
-                <p>These cute stickers are great to put on water bottles, laptops, and phone cases! We used vinyl
-                    plastic to create waterproof stickers that can endure repeated use.
-                </p>
-                <p>These sticker packs come with 30 total stickers (10 large, 10 medium, and 10 small sized stickers).
-                </p>
-                <button id="btn-add">Add to Cart</button>
-            </section>
-        </section>
-        <!-- Review box -->
-        <aside id="review-box" class="one">
-            <div>
-                <!-- User reviews boxes -->
-                <div id="reviews">
-                    <div id="review-head">
-                       
-                        <h4>Loren Isles</h4>
-                    </div>
-                    <p> These worked so well with my phone case!
-                    </p>
-                    <p id="rating">4.5/5</p>
-                </div>
-                <div id="reviews">
-                    <div id="review-head">
-                       
-                        <h4>Aubrey Lewis</h4>
-                    </div>
-                    <p> The stickers are so pretty! I wished they would come in smaller sizes though.
-                    </p>
-                    <p id="rating">4.0/5</p>
-                </div>
-                <div id="reviews">
-                    <div id="review-head">
-                    
-                        <h4>Makayla Brown</h4>
-                    </div>
-                    <p> I really liked these stickers, but I think it would be better with more designs rather than just
-                        the logo.
-                    </p>
-                    <p id="rating">4.5/5</p>
-                </div>
-            </div>
-        </aside> */
