1a19d804a1f96476170e63851344d0d6185c6bba
kuriakm <kuriakose_ma@mailfence.com>
2024-05-29 14:12:06 -0400

    Moved json folder to main directory

--- csce242/projects/part5/blog.js
+++ csce242/projects/part5/blog.js
@@ -1,55 +1,55 @@
-//https://kuriakm.github.io/csce242/json/post.json
+//https://kuriakm.github.io/json/post.json
 const getPosts = async () => {
-    try {
-        return (await fetch("https://kuriakm.github.io/csce242/json/post.json")).json();
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    return (await fetch("https://kuriakm.github.io/json/post.json")).json();
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const showPosts = async () => {
-    const posts = await getPosts();
-    const blogPosts = document.getElementById("posts");
+  const posts = await getPosts();
+  const blogPosts = document.getElementById("posts");
 
-    posts.forEach(async (post) => {
-        blogPosts.append(getBlogPost(post));
-    });
+  posts.forEach(async (post) => {
+    blogPosts.append(getBlogPost(post));
+  });
 };
 
 const getBlogPost = (post) => {
-    // Link
-    const link = document.createElement("a");
-    link.href = "post.html?seo=" + post.seo;
+  // Link
+  const link = document.createElement("a");
+  link.href = "post.html?seo=" + post.seo;
 
-    // Post section
-    const section = document.createElement("section");
-    section.classList.add("post-content");
-    link.append(section);
+  // Post section
+  const section = document.createElement("section");
+  section.classList.add("post-content");
+  link.append(section);
 
-    // Thumbnail image
-    const img = document.createElement("img");
+  // Thumbnail image
+  const img = document.createElement("img");
 
-    // Area for headline and lede (class post-text)
-    const div = document.createElement("div");
+  // Area for headline and lede (class post-text)
+  const div = document.createElement("div");
 
-    // Headline 
-    const h4 = document.createElement("h4");
+  // Headline
+  const h4 = document.createElement("h4");
 
-    // Lede
-    const p = document.createElement("p");
+  // Lede
+  const p = document.createElement("p");
 
-    // Populate post preview content
-    h4.innerHTML = post.headline;
-    p.innerHTML = post.lede;
-    img.src = "images/posts/" + post.thumbnail.name;
+  // Populate post preview content
+  h4.innerHTML = post.headline;
+  p.innerHTML = post.lede;
+  img.src = "images/posts/" + post.thumbnail.name;
 
-    // Append items to section and post-text div
-    section.append(img);
-    section.append(div);
-    div.append(h4);
-    div.append(p);
+  // Append items to section and post-text div
+  section.append(img);
+  section.append(div);
+  div.append(h4);
+  div.append(p);
 
-    return link;
-}
+  return link;
+};
 
 window.onload = showPosts();
--- csce242/projects/part6/reviews.js
+++ csce242/projects/part6/reviews.js
@@ -1,39 +1,75 @@
-// https://kuriakm.github.io/csce242/json/store-items.json
+// https://kuriakm.github.io/json/store-items.json
 const getProducts = async () => {
-    try {
-        return (await fetch("https://kuriakm.github.io/csce242/json/store-items.json")).json();
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    return (
+      await fetch("https://kuriakm.github.io/json/store-items.json")
+    ).json();
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const showProduct = async () => {
-    const products = await getProducts();
-    // const productDetails = document.getElementById("information");
+  const products = await getProducts();
+  // const productDetails = document.getElementById("information");
 
-    const reviews = document.getElementById("review-box");
+  const reviews = document.getElementById("review-box");
 
-    // Pull product name from window URL to populate store-item.html
-    const param = new URLSearchParams(window.location.search);
-    const itemType = param.get('item');
+  // Pull product name from window URL to populate store-item.html
+  const param = new URLSearchParams(window.location.search);
+  const itemType = param.get("item");
 
-    products.forEach((product) => {
-        if (product.item == itemType) {
-            const reviewDiv = document.createElement("div");
+  products.forEach((product) => {
+    if (product.item == itemType) {
+      const reviewDiv = document.createElement("div");
 
-            const reviewLength = Object.keys(product.reviews).length;
-            if (reviewLength > 0) {
-                for (let i = 0; i < reviewLength; i++) {
-                    reviewDiv.append(getReview(product.reviews[i]));
-                }
-            }
-            reviews.append(reviewDiv);
+      const reviewLength = Object.keys(product.reviews).length;
+      if (reviewLength > 0) {
+        for (let i = 0; i < reviewLength; i++) {
+          reviewDiv.append(getReview(product.reviews[i]));
         }
-    });
+      }
+      reviews.append(reviewDiv);
+    }
+  });
 };
 
 // Get review to append to review-box
 const getReview = (review) => {
+  const reviewSection = document.createElement("div");
+  reviewSection.classList.add("reviews");
+
+  const reviewHeader = document.createElement("div");
+  reviewHeader.classList.add("review-head");
+
+  const reviewer = document.createElement("h4");
+  reviewer.innerHTML = review.reviewer;
+
+  reviewHeader.append(reviewer);
+  reviewSection.append(reviewHeader);
+
+  const reviewContent = document.createElement("p");
+  reviewContent.innerHTML = review.content;
+  reviewSection.append(reviewContent);
+
+  const reviewRating = document.createElement("p");
+  reviewRating.classList.add("rating");
+  reviewRating.innerHTML = review.rating + "/5";
+  reviewSection.append(reviewRating);
+  return reviewSection;
+};
+
+// Review form submission
+document.getElementById("review-form").onsubmit = (e) => {
+  e.preventDefault();
+  const form = e.target;
+  const name = form.elements["txt-name"].value;
+  const review = form.elements["txt-com"].value;
+  const rating = form.elements["rating"].value;
+  const result = document.getElementById("result");
+
+  // Append review to review-box
+  try {
     const reviewSection = document.createElement("div");
     reviewSection.classList.add("reviews");
 
@@ -41,78 +77,42 @@ const getReview = (review) => {
     reviewHeader.classList.add("review-head");
 
     const reviewer = document.createElement("h4");
-    reviewer.innerHTML = review.reviewer;
+    reviewer.innerHTML = name;
 
     reviewHeader.append(reviewer);
     reviewSection.append(reviewHeader);
 
     const reviewContent = document.createElement("p");
-    reviewContent.innerHTML = review.content;
+    reviewContent.innerHTML = review;
     reviewSection.append(reviewContent);
 
     const reviewRating = document.createElement("p");
     reviewRating.classList.add("rating");
-    reviewRating.innerHTML = review.rating + "/5";
+    reviewRating.innerHTML = rating + "/5";
     reviewSection.append(reviewRating);
-    return reviewSection;
-}
 
-// Review form submission
-document.getElementById("review-form").onsubmit = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const name = form.elements["txt-name"].value;
-    const review = form.elements["txt-com"].value;
-    const rating = form.elements["rating"].value;
-    const result = document.getElementById("result");
+    document.getElementById("review-box").append(reviewSection);
+    result.innerHTML = "Review successfully submitted.";
 
-    // Append review to review-box
-    try {
-        const reviewSection = document.createElement("div");
-        reviewSection.classList.add("reviews");
+    // Display timeout
+    setTimeout(function () {
+      result.style.display = "none";
+    }, 2000);
+  } catch (error) {
+    console.log(error);
+    result.innerHTML = "Sorry, there was an issue submitting your review.";
 
-        const reviewHeader = document.createElement("div");
-        reviewHeader.classList.add("review-head");
-
-        const reviewer = document.createElement("h4");
-        reviewer.innerHTML = name;
-
-        reviewHeader.append(reviewer);
-        reviewSection.append(reviewHeader);
-
-        const reviewContent = document.createElement("p");
-        reviewContent.innerHTML = review;
-        reviewSection.append(reviewContent);
-
-        const reviewRating = document.createElement("p");
-        reviewRating.classList.add("rating");
-        reviewRating.innerHTML = rating + "/5";
-        reviewSection.append(reviewRating);
-
-        document.getElementById("review-box").append(reviewSection);
-        result.innerHTML = "Review successfully submitted.";
-
-        // Display timeout
-        setTimeout(function () {
-            result.style.display = "none";
-        }, 2000);
-    } catch (error) {
-        console.log(error);
-        result.innerHTML = "Sorry, there was an issue submitting your review.";
-
-        // Display timeout
-        setTimeout(function () {
-            result.style.display = "none";
-        }, 2000);
-    }
-
-
-}
+    // Display timeout
+    setTimeout(function () {
+      result.style.display = "none";
+    }, 2000);
+  }
+};
 
 window.onload = showProduct();
 
 const resetForm = () => {
-    document.getElementById("review-form").reset();
-}
+  document.getElementById("review-form").reset();
+};
 
 window.onload = resetForm();
